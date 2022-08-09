7c5362b820fc5aabf4fd235a59469ad57374775c
SANLAB\ismail.turkmenoglu <ismail.turkmenoglu@sanlab.net>
2022-08-09 18:15:59 +0300

    Init

---
+++ src/AspnetWebappWithSweetalert2/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AspnetWebappWithSweetalert2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AspnetWebappWithSweetalert2")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("cbd9ea88-1537-447e-a675-c9d2646188b7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/AspnetWebappWithSweetalert2/assets/sweetalert2/messagebox.js
@@ -0,0 +1,58 @@
+﻿class messagebox {
+    static showSuccess(title, text) {
+        Swal.fire({
+            title: title,
+            text: text,
+            icon: "success",
+            confirmButtonColor: "#3085d6",
+            confirmButtonText: "Tamam"
+        });
+    }
+
+    static showError(title, text) {
+        Swal.fire({
+            title: title,
+            text: text,
+            icon: "error",
+            confirmButtonColor: "#3085d6",
+            confirmButtonText: "Tamam"
+        });
+    }
+
+    static showWarning(title, text) {
+        Swal.fire({
+            title: title,
+            text: text,
+            icon: "warning",
+            confirmButtonColor: "#3085d6",
+            confirmButtonText: "Tamam"
+        });
+    }
+
+    static showInfo(title, text) {
+        Swal.fire({
+            title: title,
+            text: text,
+            icon: "info",
+            confirmButtonColor: "#3085d6",
+            confirmButtonText: "Tamam"
+        });
+    }
+
+    static showConfirm(title, text, confirmTitle, confirmText) {
+        Swal.fire({
+            title: title,
+            text: text,
+            icon: "warning",
+            showCancelButton: true,
+            cancelButtonColor: "#d33",
+            cancelButtonText: "Hayır",
+            confirmButtonColor: "#28A745",
+            confirmButtonText: "Evet"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                messagebox.showSuccess(confirmTitle, confirmText);
+            }
+        });
+    }
+}
